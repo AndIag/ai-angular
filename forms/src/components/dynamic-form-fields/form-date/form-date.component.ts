d58d8b50a2b559ad8bd3499e11e5c6589210ee57
Iagocanalejas <iagocanalejas@gmail.com>
2018-10-30 17:32:32 +0100

    form-date now use locale and default theme in bsDatepicker

--- forms/src/components/dynamic-form-fields/form-date/form-date.component.ts
+++ forms/src/components/dynamic-form-fields/form-date/form-date.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 import {AbstractControl, FormGroup} from '@angular/forms';
 import {Field, FieldConfig} from '../../../models/form-field';
 
@@ -8,12 +8,13 @@ import {Field, FieldConfig} from '../../../models/form-field';
   styleUrls: ['./form-date.component.scss']
 })
 export class FormDateComponent implements Field {
-  public readonly DEFAULT_LOCALE = 'en-en';
-
   public config: FieldConfig;
   public group: FormGroup;
   public control: AbstractControl;
 
-  public locale = (): string => this.config.extra && this.config.extra.locale || this.DEFAULT_LOCALE;
   public required = () => this.control && this.control.errors && this.control.errors.hasOwnProperty('required');
+
+  constructor(@Inject(LOCALE_ID) public locale: string) {
+  }
+
 }
